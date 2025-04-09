605a53d454805464ae42d0209872d64faf680bbe
deploy <baysaa41@yahoo.com>
2025-04-09 21:46:08 +0800

    update utils.py second round

--- mmo/urls.py
+++ mmo/urls.py
@@ -44,5 +44,8 @@ urlpatterns = ([
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-               + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
+])
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
