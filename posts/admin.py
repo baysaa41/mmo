d5a11677f96c86075577dfbf444fbe45fe56967b
deploy <baysaa41@yahoo.com>
2025-09-13 18:54:36 +0800

    2025 v06 code improvements, advance_grades.py

--- posts/admin.py
+++ posts/admin.py
@@ -1,10 +1,31 @@
+# posts/admin.py - САЙЖРУУЛСАН ХУВИЛБАР
+
 from django.contrib import admin
 from .models import Post
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'year', 'startdate', 'isshow', 'isspec')
-    list_filter = ('year', 'isshow', 'isspec')
-    fields = ('title', 'descr', 'year', 'startdate', 'enddate', 'isshow', 'isspec', '', 'createdate')
+    # Админы жагсаалтад харагдах хэсэг
+    list_display = ('title', 'year', 'author', 'startdate', 'isshow', 'isspec')
+    list_filter = ('year', 'isshow', 'isspec', 'author')
     search_fields = ('title', 'descr')
 
-admin.site.register(Post, PostAdmin)
+    # Шинээр нэмэх, засах хуудсыг цэгцтэй бүлгүүдэд хуваах
+    fieldsets = (
+        ('Үндсэн агуулга', {
+            'fields': ('title', 'descr', 'author')
+        }),
+        ('Тохиргоо ба Огноо', {
+            'fields': ('year', 'startdate', 'enddate', 'isshow', 'isspec')
+        }),
+        ('Нэмэлт медиа ба файлууд', {
+            'classes': ('collapse',),  # Энэ хэсгийг нууж, дарахад дэлгэгддэг болгоно
+            'fields': ('intro', 'imagesource', 'embedcode', 'pictures', 'files', 'tags')
+        }),
+        ('Системийн мэдээлэл (зөвхөн харах)', {
+            'fields': ('createdate', 'updatedate', 'sawcount', 'createuserid', 'oldid')
+        }),
+    )
+
+    # Автоматаар үүсдэг эсвэл өөрчлөх шаардлагагүй талбарууд
+    readonly_fields = ('createdate', 'updatedate', 'sawcount')
--- posts/models.py
+++ posts/models.py
@@ -2,14 +2,32 @@ from django.db import models
 from olympiad.models import SchoolYear
 from ckeditor.fields import RichTextField
 from accounts.models import Author
+from django.utils import timezone
+from datetime import date # date-г ашиглахын тулд нэмнэ
+
+# 'year' талбарын default утгыг олох туслах функц
+def get_current_school_year():
+    """Одоогийн идэвхтэй хичээлийн жилийг буцаана."""
+    # timezone.now()-г ашигласнаар таны TIME_ZONE тохиргоонд зөв ажиллана
+    today = timezone.now().date()
+    # Одоогийн огноог агуулсан хичээлийн жилийг хайх
+    school_year = SchoolYear.objects.filter(start__lte=today, end__gte=today).first()
+    return school_year
 
 class Post(models.Model):
     oldid = models.IntegerField(default=1)
     title = models.CharField(max_length=500,null=True, blank=True)
     intro = models.TextField(null=True, blank=True)
     descr = RichTextField(null=True, blank=True)
-    year = models.ForeignKey(SchoolYear, on_delete=models.SET_NULL, null=True, blank=True)
-    startdate = models.DateField(null=True, blank=True)
+
+    # --- ӨӨРЧЛӨЛТ ---
+    # default-д дээрх функцийг зааж өгсөн
+    year = models.ForeignKey(SchoolYear, on_delete=models.SET_NULL, null=True, blank=True, default=get_current_school_year)
+
+    # --- ӨӨРЧЛӨЛТ ---
+    # default-д өнөөдрийн огноог зааж өгсөн
+    startdate = models.DateField(null=True, blank=True, default=date.today)
+
     enddate = models.DateField(null=True, blank=True)
     imagesource = models.CharField(max_length=200,null=True, blank=True)
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, blank=True)
@@ -21,8 +39,14 @@ class Post(models.Model):
     sawcount = models.IntegerField(null=True, blank=True)
     isshow = models.BooleanField(default=True)
     createuserid = models.IntegerField(null=True, blank=True)
-    createdate = models.DateField(null=True, blank=True)
-    updatedate = models.DateField(null=True, blank=True)
+
+    # --- ӨӨРЧЛӨЛТ ---
+    # Анх үүсгэх үед огноог автоматаар нэмнэ
+    createdate = models.DateField(auto_now_add=True, null=True, blank=True)
+
+    # --- ӨӨРЧЛӨЛТ ---
+    # Засвар хийх бүрт огноог автоматаар шинэчилнэ
+    updatedate = models.DateField(auto_now=True, null=True, blank=True)
 
     class Meta:
         ordering = ['-year_id','-isspec']
