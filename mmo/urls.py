0dc1801cba0d700fb94cb85430bb2b29f5bec769
deploy <baysaa41@yahoo.com>
2025-09-25 19:22:11 +0800

    2025 v11, Sep 23, olympiad stats, scoresheet fix

--- mmo/urls.py
+++ mmo/urls.py
@@ -1,18 +1,10 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
-from accounts.views.auth import CustomPasswordResetView
-from django.contrib import admin
-from django.urls import include, path   # ← энд path заавал байх ёстой
 from django.contrib import admin
 from django.urls import include, path
-
-import olympiad.views
-import accounts.views as accounts_views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from accounts.views.auth import CustomPasswordResetView
+import accounts.views as accounts_views
 
 
 urlpatterns = ([
