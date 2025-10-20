3ce8613aa6bc296bb75b1da9b20f4f8fa04a60b4
deploy <baysaa41@yahoo.com>
2025-10-21 04:31:40 +0800

    2025 10 20 fix, random password

--- olympiad/views_results.py
+++ olympiad/views_results.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.db.models import Count, Avg, Max, Min
 from django.core.paginator import Paginator
-from django.utils import timezone
-from datetime import datetime
+from datetime import datetime, timezone
 from .models import Olympiad, Problem, Result, SchoolYear, ScoreSheet
 from accounts.models import Province
 from schools.models import School
