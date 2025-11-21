2cd0598049dfd911b393baf15d94c08b4a04258c
baysaa41 <baysaa41@yahoo.com>
2025-11-21 17:32:31 +0800

    11.20 major change on school admin + manager

--- olympiad/templatetags/latex_filter.py
+++ olympiad/templatetags/latex_filter.py
@@ -1,9 +1,7 @@
 from django import template
-from django.conf import settings
-import re, hashlib, io, os
+import re, hashlib
 
 register = template.Library()
-
 
 @register.filter(name='latex')
 def latex(value):
@@ -12,8 +10,7 @@ def latex(value):
     value = bf2strong(value)
     value = center2div(value)
     value = eqno2quad(value)
-    return pst2png(value)
-
+    return latex_placeholder(value)
 
 def enum2ol(value):
     value=re.sub(r'\\begin{enumerate}.*?\\item',"<ol>\n<li>",value,flags=re.DOTALL)
@@ -21,82 +18,57 @@ def enum2ol(value):
     value=re.sub(r'\\end{enumerate}','</li>\n</ol>',value)
     return value
 
-
 def emph2em(text):
     text = re.sub(r'\\emph{(.*?)}',r'<em>\1</em>',text)
     text = re.sub(r'``(.*?)\'\'',r'<q>\1</q>',text)
     return text
 
-
 def bf2strong(text):
     text = re.sub(r'\\textbf{(.*?)}',r'<strong>\1</strong>',text)
     return text
 
-
 def eqno2quad(text):
-    text.replace("eqno","quad")
-    return text
-
+    return text.replace("eqno", "quad")
 
 def center2div(text):
-    text = re.sub(r'(\\begin{center}\s+(.*?)\s+\\end{center})',r'<div class="text-center">\2</div>',text,flags=re.DOTALL)
-    return text
+    return re.sub(
+        r'(\\begin{center}\s+(.*?)\s+\\end{center})',
+        r'<div class="text-center">\2</div>',
+        text,
+        flags=re.DOTALL
+    )
 
+# --------------------------------------------------------
+# NEW FUNCTION: no TeX, no dvips, no convert → placeholder only
+# --------------------------------------------------------
+def latex_placeholder(text):
+    """
+    Converts [pst]...[/pst], [xlat]...[/xlat], [psti]...[/psti]
+    into a lightweight placeholder <div> (NO image generation).
+    """
 
-def pst2png(text):
-    TEX_ROOT = settings.TEX_ROOT
-    latex = settings.LATEX
-    dvips = settings.DVIPS
-    convert = settings.CONVERT
-    pdflatex = settings.PDFLATEX
-    os.chdir( TEX_ROOT )
-    matches=re.findall(r'\[pst\](.*?)\[/pst\]',text, re.MULTILINE|re.DOTALL)
-    for match in matches:
-        match = match.strip()
-        result = hashlib.md5(match.encode())
-        name = result.hexdigest()
-        text = text.replace(match, "<div class='picture text-center'><img src='/static/latex/{}.png'></div>".format(name))
-        if not os.path.isfile("{}/{}.png".format(TEX_ROOT,name)):
-            with io.open('{}.tex'.format(name),"w") as f:
-                print(match, file=f)
-            os.system('{} {}.tex'.format(latex, name))
-            os.system('{} {}.dvi'.format(dvips, name))
-            os.system('{} {}.ps {}.png'.format(convert, name,name))
-            #os.system('rm *.tex *.ps *.dvi *.log *.aux')
+    # PST block placeholder
+    text = re.sub(
+        r'\[pst\](.*?)\[/pst\]',
+        r"<div class='latex-placeholder'>図 зураг: PST блок</div>",
+        text,
+        flags=re.DOTALL
+    )
 
-    matches=re.findall(r'\[xlat\](.*?)\[/xlat\]',text, re.MULTILINE|re.DOTALL)
-    for match in matches:
-        match = match.strip()
-        result = hashlib.md5(match.encode())
-        name = result.hexdigest()
-        text = text.replace(match, "<div class='picture text-center'><img src='/static/latex/{}.png'></div>".format(name))
-        if not os.path.isfile("{}/{}.png".format(TEX_ROOT,name)):
-            with io.open('{}.tex'.format(name),"w") as f:
-                print(match, file=f)
-            os.system('{} {}.tex'.format(pdflatex, name))
-            os.system('{} {}.pdf {}.png'.format(convert, name,name))
+    # XLAT block placeholder
+    text = re.sub(
+        r'\[xlat\](.*?)\[/xlat\]',
+        r"<div class='latex-placeholder'>図 зураг: XLAT блок</div>",
+        text,
+        flags=re.DOTALL
+    )
 
-    matches=re.findall(r'\[psti\](.*?)\[/psti\]',text, re.MULTILINE|re.DOTALL)
-    for match in matches:
-        match = match.strip()
-        result = hashlib.md5(match.encode())
-        name = result.hexdigest()
-        text = text.replace(match, "<span class='inline-picture'><img src='/static/latex/{}.png'></span>".format(name))
-        if not os.path.isfile("{}/{}.png".format(TEX_ROOT,name)):
-            with io.open('{}.tex'.format(name),"w") as f:
-                print(match, file=f)
-            os.system('{} {}.tex'.format(latex, name))
-            os.system('{} {}.dvi'.format(dvips, name))
-            os.system('{} {}.ps {}.png'.format(convert, name,name))
-            os.system('rm *.tex *.ps *.dvi *.log *.aux')
-    text = re.sub(r'\[(/*)pst(i*)\](\n*)','',text)
-    return text
+    # PST inline placeholder
+    text = re.sub(
+        r'\[psti\](.*?)\[/psti\]',
+        r"<span class='latex-placeholder-inline'>図 inline</span>",
+        text,
+        flags=re.DOTALL
+    )
 
-
-def multicol2fix(text):
-    text.replace("[multicols=2]", "<div style='-webkit-column-count: 2;-moz-column-count: 2;column-count: 2;'>")
-    text.replace("[multicols=3]", "<div style='-webkit-column-count: 2;-moz-column-count: 2;column-count: 3;'>")
-    text.replace("[multicols=4]", "<div style='-webkit-column-count: 2;-moz-column-count: 2;column-count: 4;'>")
-    text.replace("[multicols=5]", "<div style='-webkit-column-count: 2;-moz-column-count: 2;column-count: 5;'>")
-    text.replace("[/multicols]", "</div>")
     return text
